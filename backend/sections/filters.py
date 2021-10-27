f0f5d3271d3eacee3c2d0488d52dfdea53cbe6d2
Salakhiev <2408199898@mail.ru>
2021-10-27 22:47:59 +0300

    Filter char by contains

--- backend/sections/filters.py
+++ backend/sections/filters.py
@@ -1,8 +1,10 @@
 from django_filters import rest_framework as filters
+import django_filters
 from .models import Section
 
 
 class SectionFilter(filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='contains')
 
     class Meta:
         model = Section
--- backend/students/filters.py
+++ backend/students/filters.py
@@ -1,8 +1,11 @@
 from django_filters import rest_framework as filters
+import django_filters
 from .models import Student
 
 
 class StudentFilter(filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='contains')
+    last_name = django_filters.CharFilter(lookup_expr='contains')
 
     class Meta:
         model = Student
